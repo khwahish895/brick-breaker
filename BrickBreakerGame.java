054791c32e2843e37f5ad8f3370ddbd229a4bedf
khwahish895 <khwahishsingh2005@gmail.com>
2025-06-03 16:16:06 +0530

    Add files via upload

---
+++ BrickBreakerGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brick.breaker.game;
+
+/**
+ *
+ * @author 91960
+ */
+public class BrickBreakerGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
